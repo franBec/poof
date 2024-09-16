d69ed4230047278101b02215ea6ea3f479ec08ba
Fran Bec <franbecvort@gmail.com>
2024-09-16 18:56:35 +0100

    initial commit

---
+++ src/test/java/dev/pollito/poof/PoofApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.pollito.poof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoofApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
