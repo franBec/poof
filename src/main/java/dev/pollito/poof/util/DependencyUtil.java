c897abe1b6a1993c95b5d03ea42584d871386ba1
Fran Bec <franbecvort@gmail.com>
2024-09-23 20:44:24 +0100

    WIP Whitespace and control characters in literals should be explicit

--- src/main/java/dev/pollito/poof/util/DependencyUtil.java
+++ src/main/java/dev/pollito/poof/util/DependencyUtil.java
@@ -4,5 +4,26 @@ public class DependencyUtil {
   private DependencyUtil() {}
 
   public static final String CONSUMER_EXECUTION_TEMPLATE =
-      "<execution>\r\n\t\t\t\t\t\t<id>consumer generation - <!--name--></id>\r\n\t\t\t\t\t\t<goals>\r\n\t\t\t\t\t\t\t<goal>generate</goal>\r\n\t\t\t\t\t\t</goals>\r\n\t\t\t\t\t\t<configuration>\r\n\t\t\t\t\t\t\t<inputSpec>${project.basedir}/src/main/resources/openapi/<!--name-->.yaml</inputSpec>\r\n\t\t\t\t\t\t\t<generatorName>java</generatorName>\r\n\t\t\t\t\t\t\t<library>feign</library>\r\n\t\t\t\t\t\t\t<output>${project.build.directory}/generated-sources/openapi/</output>\r\n\t\t\t\t\t\t\t<apiPackage><!--apiPackage--></apiPackage>\r\n\t\t\t\t\t\t\t<modelPackage><!--modelPackage--></modelPackage>\r\n\t\t\t\t\t\t\t<configOptions>\r\n\t\t\t\t\t\t\t\t<feignClient>true</feignClient>\r\n\t\t\t\t\t\t\t\t<interfaceOnly>true</interfaceOnly>\r\n\t\t\t\t\t\t\t\t<useEnumCaseInsensitive>true</useEnumCaseInsensitive>\r\n\t\t\t\t\t\t\t</configOptions>\r\n\t\t\t\t\t\t</configuration>\r\n\t\t\t\t\t</execution>";
+      """
+
+                              <execution>
+          						<id>consumer generation - <!--name--></id>
+          						<goals>
+          							<goal>generate</goal>
+          						</goals>
+          						<configuration>
+          							<inputSpec>${project.basedir}/src/main/resources/openapi/sample name.yaml</inputSpec>
+          							<generatorName>java</generatorName>
+          							<library>feign</library>
+          							<output>${project.build.directory}/generated-sources/openapi/</output>
+          							<apiPackage><!--apiPackage--></apiPackage>
+          							<modelPackage><!--modelPackage--></modelPackage>
+          							<configOptions>
+          								<feignClient>true</feignClient>
+          								<interfaceOnly>true</interfaceOnly>
+          								<useEnumCaseInsensitive>true</useEnumCaseInsensitive>
+          							</configOptions>
+          						</configuration>
+          					</execution>
+          """;
 }
